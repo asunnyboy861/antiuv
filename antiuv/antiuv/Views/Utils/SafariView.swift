046d9ff3f1a051e00e4549e649c845fb7247b0e1
asunnyboy861 <asunnyboy861@gmail.com>
2026-03-12 21:40:18 +0800

    fix: Safari页面左上角显示英文Done按钮
    - 创建自定义CustomSafariViewController
    - 添加英文Done按钮替代系统默认按钮
    - 实现SFSafariViewControllerDelegate处理页面关闭

--- antiuv/antiuv/Views/Utils/SafariView.swift
+++ antiuv/antiuv/Views/Utils/SafariView.swift
@@ -11,13 +11,58 @@ import SafariServices
 struct SafariView: UIViewControllerRepresentable {
     let url: URL
     
-    func makeUIViewController(context: Context) -> SFSafariViewController {
-        let configuration = SFSafariViewController.Configuration()
-        configuration.entersReaderIfAvailable = false
-        let safariViewController = SFSafariViewController(url: url, configuration: configuration)
-        safariViewController.preferredControlTintColor = .orange
-        return safariViewController
+    func makeUIViewController(context: Context) -> CustomSafariViewController {
+        return CustomSafariViewController(url: url)
     }
     
-    func updateUIViewController(_ uiViewController: SFSafariViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: CustomSafariViewController, context: Context) {}
+}
+
+class CustomSafariViewController: UIViewController {
+    private var safariViewController: SFSafariViewController?
+    
+    init(url: URL) {
+        super.init(nibName: nil, bundle: nil)
+        
+        let configuration = SFSafariViewController.Configuration()
+        configuration.entersReaderIfAvailable = false
+        configuration.barCollapsingEnabled = false
+        
+        let safariVC = SFSafariViewController(url: url, configuration: configuration)
+        safariVC.preferredControlTintColor = .orange
+        safariVC.delegate = self
+        
+        self.safariViewController = safariVC
+        self.addChild(safariVC)
+        self.view.addSubview(safariVC.view)
+        safariVC.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            safariVC.view.topAnchor.constraint(equalTo: self.view.topAnchor),
+            safariVC.view.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            safariVC.view.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            safariVC.view.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+        safariVC.didMove(toParent: self)
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "Done",
+            style: .done,
+            target: self,
+            action: #selector(doneButtonTapped)
+        )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc private func doneButtonTapped() {
+        self.dismiss(animated: true)
+    }
+}
+
+extension CustomSafariViewController: SFSafariViewControllerDelegate {
+    func safariViewControllerDidFinish(_ controller: SFSafariViewController) {
+        self.dismiss(animated: true)
+    }
 }
